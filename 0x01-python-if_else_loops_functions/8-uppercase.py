0ee96c837934e7baf46b759a3cc1243414b4a4b4
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-07-04 04:49:01 -0700

    fixed bug

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    ascii_values = [chr(ord(c)-32) for c in str]
-    print("{}".format(ascii_values), end="")
+    tmp = list(str)
+
+    for i in range(len(tmp)):
+        if ord(tmp[i]) > 96 and ord(tmp[i]) < 123:
+            tmp[i] = chr(ord(tmp[i])-32)
+        
+    print("{}".format("".join(tmp)))
