160990ff00ff429e7dc742e5cae57ad567a1d55a
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-07-05 08:07:04 -0700

    removed characters from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     str = ''
-    new_string = my_string.translate({ord(i):str for i in 'cC'})
+    new_string = my_string.translate({ord(i): str for i in 'cC'})
     return new_string
