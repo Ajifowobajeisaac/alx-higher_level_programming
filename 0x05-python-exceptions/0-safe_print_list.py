bd25811e88957ae50fff7b71528f9929648a3083
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-07-18 18:17:50 -0700

    removed test folder

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,12 @@
 def safe_print_list(my_list=[], x=0):
     '''Print a list of x elements'''
 
-    counter = 0
+    ret = 0
     for i in range(x):
         try:
-            print(my_list[i], end='')
-            counter += 1
+            print("{}".format(my_list[i]), end="")
+            ret += 1
         except IndexError:
             break
     print("")
-    return (counter)
+    return (ret)
