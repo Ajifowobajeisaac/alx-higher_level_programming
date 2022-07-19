9b5d344bc0f20e488b9431d6f979113f21299419
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-07-18 21:05:55 -0700

    added function to print integers in a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,3 +2,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     ''' a function that prints the first x elements of a list and only integers.'''
+    count = 0
+
+    for i in range(x):
+        while x == int:
+            try:
+                print("{:d}".format(i))
+            except: (TypeError, ValueError):
+                break
+    print("")
+    return counter
