8c5bd36954b0637c116b654f9a8a60d297938682
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-07-18 17:28:04 -0700

    fixed bugs

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,14 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    
+    ''' Prints a list with error handling and exceptions'''
+    counter = 0
+    i = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+            counter += 1
+        except IndexError:
+            break
+        print("")
+        return counter
