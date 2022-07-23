9101b8e142398659c4f6ebb83f1cb92fa0a197ee
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-07-23 04:09:26 -0700

    defined a square with a class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+"""Defines a square and it's attributes"""
+
+class Square:
+    """A square and it's attributes"""
+
+    def __init__(self, size):
+        self.__size = size
