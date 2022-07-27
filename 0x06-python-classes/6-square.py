806009cd0269cf448854bcce1f3106fe4ee92289
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-07-27 07:00:22 -0700

    removed space in print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,7 +51,7 @@ class Square:
 
     def my_print(self):
         if self.__size < 1:
-            print(" ")
+            print("")
         else:
             [print("") for i in range(0, self.__position[1])]
             for i in range(0, self.__size):
