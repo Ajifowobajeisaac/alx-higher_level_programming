a83e37c266948f6b2df27c58930edd5e94ef7cd6
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-07-27 16:47:03 -0700

    updated rectangle class with width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""A class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialises a new rectangle
+        
+        args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+            """
+        
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """int: Get/set the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("witdth must be >= 0")
+
+    @property
+    def height(self):
+        """int: Get/set the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
