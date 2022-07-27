6e8ed032bed85ca7f91ed4e4342ccd9f0fa212f3
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-07-26 19:04:22 -0700

    Updated square class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -2,40 +2,58 @@
 
 """A class that defines a square"""
 
-
-class Sqaure:
+class Square:
     """Represents a square"""
 
-    def __init__(self, size=0, postition=(0, 0)):
-        """Initialises a new square"""
-
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialises a new square.
+        
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of a new squre.
+        """
         self.__size = size
         self.__position = position
 
     @property
     def size(self):
-        """int: The size of the current square"""
-
+        """Get/set The size of the current square"""
         return self.__size
- 
+
     @size.setter
     def size(self, value):
 
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0 :
-            raise ValueError("size musst be >= 0")
+        elif value < 0:
+            raise ValueError("sie must be >= 0")
 
         self.__size = value
 
     @property
     def position(self):
-        """tuple: The position of the square"""
-
+        """str: returns an empty string"""
         return self.__position
 
     @position.setter
     def position(self, value):
+        if  (not isinstance(value, tuple) or
+                 len(value) != 2 or
+                not all(isinstance(num, int)) for num in value or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        
+        self.__position = value
 
-        if not isinstance(value, tuple):
-            raise TypeError("position must be tuple of 2 positive integers")
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size < 1:
+            print(" ")
+        else:
+            [print("") for num in range(0, self.__position[1])]
+            for i in range(self.__size):
+                [print(" ", end="") for i in range(0, self.__position[0])]
+                [print("#", end="") for j in range(0, self.__size)]
+                print("")
