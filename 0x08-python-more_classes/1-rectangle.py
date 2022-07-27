4c6817bbac2f3e24aaa3428896787d510a225602
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-07-27 16:53:49 -0700

    fixed bugs

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,12 +8,12 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialises a new rectangle
-        
+
         args:
             width (int): The width of the rectangle
             height (int): The height of the rectangle
             """
-        
+
         self.__width = width
         self.__height = height
 
@@ -29,6 +29,8 @@ class Rectangle:
         elif value < 0:
             raise ValueError("witdth must be >= 0")
 
+        self.__width = value
+
     @property
     def height(self):
         """int: Get/set the height of the rectangle"""
@@ -40,3 +42,5 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
+
+        self.__height = value
