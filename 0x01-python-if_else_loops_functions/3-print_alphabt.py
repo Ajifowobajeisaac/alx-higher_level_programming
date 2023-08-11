285c40a812eb5230733c3a94965cd4542a236867
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-08-11 06:35:41 -0700

    fixed bugs

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-#i = 97
-#while i < 123:
-#    if i == 101:
-#        continue
-#    print(f"{i} = {chr(i)}")
-#    i = i + 1
-
 for i in [chr(i) for i in range(97, 123) if i !=101 if i != 113]:
     print('{}'.format(i), end='')
