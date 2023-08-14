1eaa33c1e08c5c0696a751fcd1696208d5330822
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-08-14 02:32:29 +0100

    fixed bug

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,10 @@
 
 import sys
 
-args = sys.argv
 
-args_int = [int(arg) for arg in args[1:]]
-args_sum = sum(args_int)
-print(args_sum)
+
+if __name__ == "__main__":
+    args = sys.argv
+    args_int = [int(arg) for arg in args[1:]]
+    args_sum = sum(args_int)
+    print(args_sum)
