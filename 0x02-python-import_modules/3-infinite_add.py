42341945342371fd8596bd999cb5dd244eb759b3
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-08-14 04:52:08 +0100

    Added basic command line calculator

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,4 @@ import sys
 if __name__ == "__main__":
     args = sys.argv
     args_int = [int(arg) for arg in args[1:]]
-    args_sum = sum(args_int)
-    print(args_sum)
+    print(sum(args_int))
