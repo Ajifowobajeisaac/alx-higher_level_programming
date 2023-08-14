ea1fe8a04c0d29f4b93b63850f363e4bbdc2a2ef
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-08-13 20:01:01 -0700

    solved 'hidden' task

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+import hidden_4
 
+if __name__ == "__main__":
+    hidden_list = dir(hidden_4)
+
+    for name in hidden_list:
+        if name[:2] != '__':
+            print(name)
