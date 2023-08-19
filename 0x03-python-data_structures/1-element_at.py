1edd8df2fe6d67df3148e44bda4c2cf8fa2dd1c1
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-08-19 01:29:12 -0700

    verified list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,9 +5,10 @@
 
 def element_at(my_list, idx):
     """Retrieves an element from a list at a specific index"""
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
-        return None
-    else:
-        return my_list[idx]
+    if my_list:
+        if idx < 0:
+            return None
+        elif idx > len(my_list):
+            return None
+        else:
+            return my_list[idx]
