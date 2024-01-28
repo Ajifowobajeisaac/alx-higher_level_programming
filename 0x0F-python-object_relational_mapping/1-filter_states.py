766afca1b4fb561522bc351e7483ec144ba78dbb
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-01-28 12:14:35 +0000

    added first letter filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" A script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa."""
+
+import MySQLdb
+import sys
+
+
+def filter_states(username, password, db_name):
+    """Takes user credentials, filters a database and returns state"""
+
+    db = MySQLdb.connect(
+        host='localhost', port=3306, user=username,
+        passwd=password, db=db_name)
+    cur = db.cursor()
+
+    cur.execute('SELECT * FROM states WHERE name LIKE \'N%\'')
+
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+    filter_states(username, password, db_name)
