b6c2436fafd0046b4d34978b922140680133f302
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-01-28 11:17:23 +0000

    MySQLdb practice script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
-from sqlalchemy import create_engine
 import MySQLdb
+import sys
 
-engine = create_engine(MySQLdb, )
+# print("success")
+
+def db_mysql(username, password, database_name):
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                    passwd=password, db=database_name)
+    
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY id ASC;')
+    states = cur.fetchall()
+    
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+    
+    db_mysql(username, password, database_name)
