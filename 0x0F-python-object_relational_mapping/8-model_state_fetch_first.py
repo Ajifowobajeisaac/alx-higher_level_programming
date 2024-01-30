17cde6a6dc321affbdd48829418364b37e5fc68b
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-01-30 00:37:40 +0000

    first state filter

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""A script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from sqlalchemy import create_engine, select
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(argv) == 4:
+        username = argv[1]
+        password = argv[2]
+        db_base = argv[3]
+        port = 33
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:{}/{}'
+        .format(username, password, port, db_base), pool_pre_ping=True)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    state = State()
+    first_state = session.query(State).first()
+    print(f"{first_state.id}: {first_state.name}")
