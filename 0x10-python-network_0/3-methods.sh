6f4ad74f455a11ad795faeff942fea9d3526d623
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-02-25 09:07:06 +0000

    made script fixes

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a delete request to a URL and displays the body of the responsee
-curl -X OPTIONS -i $1 | grep -oP '(?<=Allow: )[^/r/n]*'
+curl -sX OPTIONS -i $1 | grep -oP '(?<=Allow: )[^/r/n]*'
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a getrequest with a specified header varaible to a URL and displays the body of the responsee
-curl -H "X-School-User-Id: 98" $1'
+curl -sH "X-School-User-Id: 98" $1
