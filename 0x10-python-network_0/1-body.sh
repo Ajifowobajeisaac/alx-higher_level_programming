5eb54488c2a23038f5d6cfe0630a81a09054a0c1
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-02-25 08:13:18 +0000

    reduced script length

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL and display the size of the response body
-# only if it is 200 status
+# sends a request to a URL and display the size of the response bodynly if it is 200 status
 curl -fL $1
