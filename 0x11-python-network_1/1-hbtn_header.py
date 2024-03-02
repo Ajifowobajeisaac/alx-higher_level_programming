15a27174e7e8da7e469a77baa019f30ac87991a8
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-03-02 09:07:22 +0000

    added header fetch script

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+- script that takes in a URL
+- sends a request to the URL
+- displays the value of the X-Request-Id variable found in the header of the response.
+"""
+
+if __name__ == "__main__":
+	import sys
+	import urllib.request
+
+	url = sys.argv[1]
+
+	with urllib.request.urlopen(url) as response:
+
+		x_req = response.headers.get('X-Request-Id')
+		print(x_req)
