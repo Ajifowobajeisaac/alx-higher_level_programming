05f1ce781e12eb6174d605fb4808aa5a822b8e9a
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-03-18 12:34:36 +0000

    as

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,11 +3,11 @@
 - Takes a url and an email.
 - Sends a POST request to the url with the email as parameter.
 """
+import urllib.parse
+import urllib.request
+from sys
 
 if __name__ == "__main__":
-	import urllib.parse
-	import urllib.request
-	from sys
 
 	url = sys.argv[1]
 	email = sys.argv[2]}
